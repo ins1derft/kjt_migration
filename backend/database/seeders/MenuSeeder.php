5de778e4bc3bf1e2291d3ccc6cc74a5ec6a6666a
ins1derft <grantur5707@yandex.ru>
2025-11-29 21:55:56 +0000

    feat: add nested products/experiences/services dropdown

--- backend/database/seeders/MenuSeeder.php
+++ backend/database/seeders/MenuSeeder.php
@@ -28,43 +28,123 @@ class MenuSeeder extends Seeder
     {
         $menu->items()->delete();
 
-        $topPrimary = [
+        $position = 1;
+
+        $create = function (array $data) use ($menu, &$position) {
+            return MenuItem::create([
+                ...$data,
+                'menu_id' => $menu->id,
+                'position' => $position++,
+                'is_active' => true,
+            ]);
+        };
+
+        // Top bars
+        foreach ([
             ['label' => 'News', 'url' => '/news', 'slot' => 'top_primary'],
             ['label' => 'Case Studies', 'url' => '/case-studies', 'slot' => 'top_primary'],
             ['label' => 'Testimonials', 'url' => '/news', 'slot' => 'top_primary'],
-        ];
+        ] as $item) {
+            $create($item);
+        }
 
-        $topSecondary = [
+        foreach ([
             ['label' => 'FAQs', 'url' => '/news', 'slot' => 'top_secondary'],
             ['label' => 'Support', 'url' => 'mailto:support@kidsjumptech.com', 'slot' => 'top_secondary'],
-        ];
+        ] as $item) {
+            $create($item);
+        }
 
-        $social = [
+        foreach ([
             ['label' => 'Facebook', 'url' => 'https://www.facebook.com/kidsjumptech/', 'slot' => 'social', 'icon' => 'f', 'opens_in_new_tab' => true],
             ['label' => 'Instagram', 'url' => 'https://www.instagram.com/kidsjumptech/', 'slot' => 'social', 'icon' => 'ig', 'opens_in_new_tab' => true],
             ['label' => 'LinkedIn', 'url' => 'https://www.linkedin.com/company/kidsjumptech/', 'slot' => 'social', 'icon' => 'in', 'opens_in_new_tab' => true],
             ['label' => 'YouTube', 'url' => 'https://www.youtube.com/channel/UCXwxinewM8-6sC7ltS37LXw', 'slot' => 'social', 'icon' => 'yt', 'opens_in_new_tab' => true],
+        ] as $item) {
+            $create($item);
+        }
+
+        // Primary nav
+        $home = $create(['label' => 'Home', 'url' => '/', 'slot' => 'primary']);
+        $productsExperiences = $create(['label' => 'Products & Experiences', 'url' => '/games', 'slot' => 'primary']);
+        $create(['label' => 'Industries', 'url' => '/case-studies', 'slot' => 'primary']);
+        $create(['label' => 'Why Us', 'url' => '/news', 'slot' => 'primary']);
+        $create(['label' => 'Contact', 'url' => '#contact', 'slot' => 'primary']);
+
+        // Dropdown groups under Products & Experiences
+        $productsGroup = $create([
+            'label' => 'Products',
+            'url' => '/games',
+            'slot' => 'primary',
+            'parent_id' => $productsExperiences->id,
+        ]);
+
+        $experiencesGroup = $create([
+            'label' => 'Experiences',
+            'url' => '/games',
+            'slot' => 'primary',
+            'parent_id' => $productsExperiences->id,
+        ]);
+
+        $servicesGroup = $create([
+            'label' => 'Services',
+            'url' => '/games',
+            'slot' => 'primary',
+            'parent_id' => $productsExperiences->id,
+        ]);
+
+        $productsLinks = [
+            ['label' => 'Interactive Floor', 'url' => '/interactive-floor/'],
+            ['label' => 'Mobile Interactive Floor', 'url' => '/interactive-floor-mobil/'],
+            ['label' => 'Interactive Wall', 'url' => '/interactive-throw-wall/'],
+            ['label' => 'Mobile Interactive Wall', 'url' => '/mobile-interactive-ball-wall/'],
+            ['label' => 'Interactive AR Sandbox', 'url' => '/interactive-sandbox/'],
+            ['label' => 'Multi-Touch Screen Tables', 'url' => '/interactive-multitouch-tables/'],
+            ['label' => 'Interactive Shooting', 'url' => '/interactive-shooting-gallery/'],
+            ['label' => 'Interactive Swing', 'url' => '/interactive-swing/'],
+            ['label' => 'Alive Sketch', 'url' => '/alive-draw/'],
+            ['label' => 'Interactive Climbing', 'url' => '/interactive-climbing-wall/'],
+            ['label' => 'Interactive Slide', 'url' => '/interactive-slider/'],
+            ['label' => 'Animated Drawing', 'url' => '/animated-drawing/'],
+            ['label' => 'Interactive Projector', 'url' => '/interactive-game-projector/'],
         ];
 
-        $primary = [
-            ['label' => 'Home', 'url' => '/', 'slot' => 'primary'],
-            ['label' => 'Products & Experiences', 'url' => '/games', 'slot' => 'primary'],
-            ['label' => 'Industries', 'url' => '/case-studies', 'slot' => 'primary'],
-            ['label' => 'Why Us', 'url' => '/news', 'slot' => 'primary'],
-            ['label' => 'Contact', 'url' => '#contact', 'slot' => 'primary'],
+        foreach ($productsLinks as $link) {
+            $create([
+                ...$link,
+                'slot' => 'primary',
+                'parent_id' => $productsGroup->id,
+            ]);
+        }
+
+        $experienceLinks = [
+            ['label' => 'Special Needs', 'url' => '/special-needs/'],
+            ['label' => 'Sensory Room', 'url' => '/sensory-room/'],
+            ['label' => 'Interactive Digital Parks', 'url' => '/interactive-digital-parks/'],
+            ['label' => 'Interactive Playground', 'url' => '/interactive-playground/'],
+            ['label' => 'Games and Activities', 'url' => '/games/'],
+            ['label' => 'Game Creator', 'url' => '/game-creator/'],
         ];
 
-        $position = 1;
+        foreach ($experienceLinks as $link) {
+            $create([
+                ...$link,
+                'slot' => 'primary',
+                'parent_id' => $experiencesGroup->id,
+            ]);
+        }
 
-        foreach ([$topPrimary, $topSecondary, $social, $primary] as $group) {
-            foreach ($group as $item) {
-                MenuItem::create([
-                    ...$item,
-                    'menu_id' => $menu->id,
-                    'position' => $position++,
-                    'is_active' => true,
-                ]);
-            }
+        $servicesLinks = [
+            ['label' => 'Custom Software Development', 'url' => '/custom-software-development/'],
+            ['label' => 'Product Support', 'url' => '/product-support/'],
+        ];
+
+        foreach ($servicesLinks as $link) {
+            $create([
+                ...$link,
+                'slot' => 'primary',
+                'parent_id' => $servicesGroup->id,
+            ]);
         }
     }
 
