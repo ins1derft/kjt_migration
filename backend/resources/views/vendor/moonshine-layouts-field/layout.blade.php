3c14ac2d0269eaa7be5c362f857399bd65153c6f
ins1derft <grantur5707@yandex.ru>
2025-12-08 16:55:25 +0000

    Fix accordion for Pages

---
+++ backend/resources/views/vendor/moonshine-layouts-field/layout.blade.php
@@ -0,0 +1,13 @@
+<x-moonshine::collapse
+    class="_layouts-block"
+    :persist="false"
+    :open="false"
+>
+    <x-slot name="title">
+        {!! $heading !!}
+    </x-slot>
+
+    <x-slot name="button">{!! $button !!}</x-slot>
+
+    {!! $fields !!}
+</x-moonshine::collapse>
